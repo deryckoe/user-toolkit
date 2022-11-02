c190b4734590acaeb77c125c6d765d04aab58dc6
deryck <deryck@dygo.cl>
2022-11-02 14:46:16 -0300

    webpck ready

--- src/UserTools.php
+++ src/UserTools.php
@@ -42,8 +42,8 @@ class UserTools {
 	}
 
 	function assets() {
-		wp_enqueue_script( 'user-toolkit', UT_URL . 'assets/app.js', [ 'wp-api' ], UT_VERSION, true );
-		wp_enqueue_style( 'user-toolkit', UT_URL . 'assets/app.css', [], UT_VERSION );
+		wp_enqueue_script( 'user-toolkit', UT_URL . 'assets/dist/app.js', [ 'wp-api' ], UT_VERSION, true );
+		wp_enqueue_style( 'user-toolkit', UT_URL . 'assets/dist/app.css', [], UT_VERSION );
 	}
 
 }
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./assets/src/app.js",
+  output: {
+    path: path.resolve(__dirname, "assets/dist"),
+    filename: "app.js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "app.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
